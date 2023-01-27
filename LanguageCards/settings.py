b1fef00772bc05723d574f8300492b6d2336e4bf
Lalibuck <a.buiko96@gmail.com>
2023-01-27 14:19:31 +0300

    3.10

--- LanguageCards/settings.py
+++ LanguageCards/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t4u&kf_k62kq)t0bns_=o=5vmolxr5xlojz9t6&#fo%*nkbmpb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://language-cards.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn manage.py runserver
+web: gunicorn .wsgi
+python manage.py collectstatic --noinput
+manage.py migrate
